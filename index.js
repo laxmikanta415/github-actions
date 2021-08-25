beb42de68bda2f767e28cb1fa12642e8025e8325
Laxmikanta Nayak <laxmikantanayak415@gmail.com>
2021-08-25 12:11:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 app.use('/', (req, res) =>{
-  res.status(200).json({
-  "name": process.env.NAME,
-  "region": process.env.REGION
-  })
+  let response = {
+    dummy:'go'
+  };
+  try{
+    response =   {
+      "name": process.env.NAME,
+      "region": process.env.REGION
+    }
+    console.log(process.env)
+  }catch(err){
+    console.log(err)
+  }
+  res.status(200).json(response)
 })
 app.listen(8080,function(){
   console.log('running on 8080')
