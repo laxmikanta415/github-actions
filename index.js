90b584fad90a00dcbb8cab15a6f8e09a2e611f42
Laxmikanta Nayak <laxmikantanayak415@gmail.com>
2021-08-25 11:38:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 app.use('/', (req, res) =>{
   res.status(200).json({
-  "name": "laxmikanta"
+  "name": process.env.NAME,
+  "region": process.env.REGION
   })
 })
 app.listen(8080,function(){
